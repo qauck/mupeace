14ea4d7aa4d7db9ffd1de6e11dec62aeced4bb93
David Eisner <david.eisner@oriel.oxon.org>
2015-05-19 13:24:17 +0100

    Ignore gesture calculation errors

--- main/src/org/musicpd/android/fragments/GestureFragment.java
+++ main/src/org/musicpd/android/fragments/GestureFragment.java
@@ -51,17 +51,20 @@ public class GestureFragment extends SherlockFragment {
 
 	        @Override
 	        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-	            float distanceX = e2.getX() - e1.getX();
-	            float distanceY = e2.getY() - e1.getY();
-	            if (Math.abs(distanceX) > Math.abs(distanceY)
-            		&& Math.abs(distanceX) > SWIPE_DISTANCE_THRESHOLD
-            		&& Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-	                if (distanceX > 0)
-	                    onSwipeRight();
-	                else
-	                    onSwipeLeft();
-	                return true;
-	            }
+	        	try
+	        	{
+		            float distanceX = e2.getX() - e1.getX();
+		            float distanceY = e2.getY() - e1.getY();
+		            if (Math.abs(distanceX) > Math.abs(distanceY)
+	            		&& Math.abs(distanceX) > SWIPE_DISTANCE_THRESHOLD
+	            		&& Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+		                if (distanceX > 0)
+		                    onSwipeRight();
+		                else
+		                    onSwipeLeft();
+		                return true;
+		            }
+	        	} catch(Exception e) { }
 	            return false;
 	        }
 		});
